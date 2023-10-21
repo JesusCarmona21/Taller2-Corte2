0d49645e67bb7724afdd59f5cc8ed2264894c99d
JesusCarmona21 <jesus.carmonape@cecar.edu.co>
2023-10-20 23:41:39 -0500

    new

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -13,12 +13,12 @@ function cargarImagen(id, src, width, height) {
 }
 }
 
-cargarImagen('img', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
-cargarImagen('img1', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
-cargarImagen('img2', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
-cargarImagen('img3', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
-cargarImagen('img4', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
-cargarImagen('img5', 'https://ichef.bbci.co.uk/news/640/cpsprodpb/238D/production/_95410190_gettyimages-488144002.jpg', '350px', '300px');
+cargarImagen('img', 'https://ca-times.brightspotcdn.com/dims4/default/da06139/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F52%2Fad%2F65a1ea8a41718ef56032d9e14302%2Fdisney-with-studio-tag-copy.jpg', '350px', '300px');
+cargarImagen('img1', 'https://static-assets.bamgrid.com/product/starplus/images/share-default.d72cf588f6d06cba22171f5ae44289d3.png', '350px', '300px');
+cargarImagen('img2', 'https://cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/2JA7URADWJDW7LWZQNZJNHG3PA.png', '350px', '300px');
+cargarImagen('img3', 'https://i.blogs.es/d4febd/paramount/450_1000.webp', '350px', '300px');
+cargarImagen('img4', 'https://yt3.googleusercontent.com/pn_3JEt2nFaRA6dY08NzFM2w8A7NtUbaniamEnObxtLX3ZhT9w41KW0W0pjl-RiczGS0rgzZ=s900-c-k-c0x00ffffff-no-rj', '350px', '300px');
+cargarImagen('img5', 'https://img.asmedia.epimg.net/resizer/4RsGxVvWp3h6-t9q3AM31tayO9M=/360x203/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/4CZOEZ7ESVPNHIH5KPDR2XCNB4.jpg', '350px', '300px');
 
 
 function divs(id, contenido) {
